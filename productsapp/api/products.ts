b54673635ec87440f9cc418521f40816af91a0b8
Nathan Allen <nathanallen888@gmail.com>
2017-06-15 17:17:07 -0500

    first commit

---
+++ productsapp/api/products.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import database from '../db';
+
+let router = express.Router();
+
+// GET products
+router.get('/', (req, res) => {
+  database.db.collection('products').find().toArray().then((products)=>{
+    res.json(products);
+  })
+});
+
+export default router;
